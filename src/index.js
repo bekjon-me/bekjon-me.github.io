b1ee255538af9777726ac0f013cd793727d7d7d9
Bekjon <bekjonishpulatov8@gmail.com>
2023-02-15 16:36:17 +0500

    Add linters

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-var i = 0;
-var txt = 'Welcome to our online bootcamp'; /* The text */
-var speed = 100; /* The speed/duration of the effect in milliseconds */
+let i = 0;
+const txt = 'Welcome to our online bootcamp'; /* The text */
+const speed = 100; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
   if (i < txt.length) {
     document.getElementById('typingTitle').innerHTML += txt.charAt(i);
-    i++;
+    i += 1;
     setTimeout(typeWriter, speed);
   }
 }
