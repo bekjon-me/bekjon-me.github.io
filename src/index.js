71b541ad93180d5eff9fba9ebb5710019316c63b
Bekjon <bekjonishpulatov8@gmail.com>
2023-02-15 17:11:01 +0500

    Fix

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ let i = 0;
 const txt = 'Welcome to our online bootcamp'; /* The text */
 const speed = 100; /* The speed/duration of the effect in milliseconds */
 
+let ghpages = require('gh-pages');
+
+ghpages.publish(
+  '.',
+  {
+    repo: 'git@github.com:bekjon-me/capstone-project.git',
+  },
+  callback
+);
+
 function typeWriter() {
   if (i < txt.length) {
     document.getElementById('typingTitle').innerHTML += txt.charAt(i);
